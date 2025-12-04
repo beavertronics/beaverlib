2cc37ef232b20bc9f16f1bc57bc5054341a2c769
BubbleShade <ozypfl@gmail.com>
2025-12-03 17:46:38 -0800

    Remove Async Code

--- async/Thenable.kt
+++
@@ -1,8 +0,0 @@
-package frc.beaverlib.async
-
-@Deprecated("Thenable is deprecated, if you need to use it, idk why you would need to")
-interface Thenable<T> {
-    fun<N> then(success: (value: T) -> Promise<N>): Promise<N>
-    fun<N> then(success: (value: T) -> Promise<N>, failure: (error: Throwable) -> Promise<N>) : Promise<N>
-    fun catch(failure: (error: Throwable) -> Promise<T>): Promise<T>
-}
