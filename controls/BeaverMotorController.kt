6684022e8f2f34393fdbae6428f997c518d359b5
BubbleShade <ozypfl@gmail.com>
2026-01-13 16:52:00 -0800

    Updated for 2026

--- controls/BeaverMotorController.kt
+++ controls/BeaverMotorController.kt
@@ -12,40 +12,59 @@ import com.revrobotics.spark.config.SparkBaseConfig
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode
 import com.revrobotics.spark.config.SparkMaxConfig
 
-
+@Deprecated("Deprecated as both 2898 and 5970 now have access to enough sparks to not need this")
 interface BeaverMotorController {
     /** The percent output of the motor (-1.0.. 1.0 */
-    fun get() : Double
+    fun get(): Double
 
-    /** Runs the motor connected to the controller at the voltage "volts"
-     *  (WARNING: May be unpredictable with non SparkMax controllers.)
-     *  @param volts Voltage to run the motor at */
-    fun setVoltage(volts : VoltageUnit)
-    fun configure(idleMode: BeaverIdleMode, currentLimit : Int)
+    /**
+     * Runs the motor connected to the controller at the voltage "volts" (WARNING: May be
+     * unpredictable with non SparkMax controllers.)
+     *
+     * @param volts Voltage to run the motor at
+     */
+    fun setVoltage(volts: VoltageUnit)
+
+    fun configure(idleMode: BeaverIdleMode, currentLimit: Int)
 }
-enum class BeaverIdleMode(val SparkValue : SparkBaseConfig.IdleMode, val SRXValue : NeutralMode) {
+
+@Deprecated("Deprecated as both 2898 and 5970 now have access to enough sparks to not need this")
+enum class BeaverIdleMode(val SparkValue: SparkBaseConfig.IdleMode, val SRXValue: NeutralMode) {
     UNSET(SparkBaseConfig.IdleMode.kCoast, NeutralMode.Coast),
     BREAK(SparkBaseConfig.IdleMode.kBrake, NeutralMode.Brake),
-    COAST(SparkBaseConfig.IdleMode.kCoast, NeutralMode.Coast)}
+    COAST(SparkBaseConfig.IdleMode.kCoast, NeutralMode.Coast),
+}
 
-class BeaverSparkMax(ID : Int, MotorType: SparkLowLevel.MotorType) : BeaverMotorController {
-    var motorController : SparkMax = SparkMax(ID, MotorType)
+@Deprecated("Deprecated as both 2898 and 5970 now have access to enough sparks to not need this")
+class BeaverSparkMax(ID: Int, MotorType: SparkLowLevel.MotorType) : BeaverMotorController {
+    var motorController: SparkMax = SparkMax(ID, MotorType)
+
     override fun get() = motorController.get()
+
     override fun setVoltage(volts: VoltageUnit) = motorController.setVoltage(volts.asVolts)
-    override fun configure(idleMode: BeaverIdleMode, currentLimit : Int) {
-        val sparkConfig : SparkMaxConfig = SparkMaxConfig();
-        sparkConfig.idleMode(IdleMode.kCoast);
+
+    override fun configure(idleMode: BeaverIdleMode, currentLimit: Int) {
+        val sparkConfig: SparkMaxConfig = SparkMaxConfig()
+        sparkConfig.idleMode(IdleMode.kCoast)
         sparkConfig.smartCurrentLimit(currentLimit)
         // Don't persist parameters since it takes time and this change is temporary
-        motorController.configure(sparkConfig, SparkBase.ResetMode.kNoResetSafeParameters, PersistMode.kNoPersistParameters);
+        motorController.configure(
+            sparkConfig,
+            SparkBase.ResetMode.kNoResetSafeParameters,
+            PersistMode.kNoPersistParameters,
+        )
     }
 }
 
+@Deprecated("Deprecated as both 2898 and 5970 now have access to enough sparks to not need this")
 class BeaverTalonSRX(ID: Int) : BeaverMotorController {
-    var motorController : TalonSRX = TalonSRX(ID)
+    var motorController: TalonSRX = TalonSRX(ID)
 
     override fun get() = motorController.motorOutputPercent
-    override fun setVoltage(volts: VoltageUnit) = motorController.set(TalonSRXControlMode.PercentOutput, volts.asVolts/12)
+
+    override fun setVoltage(volts: VoltageUnit) =
+        motorController.set(TalonSRXControlMode.PercentOutput, volts.asVolts / 12)
+
     override fun configure(idleMode: BeaverIdleMode, currentLimit: Int) {
         motorController.configFactoryDefault()
         motorController.configContinuousCurrentLimit(currentLimit)
--- odometry/BeaverPhotonVision.kt
+++ odometry/BeaverPhotonVision.kt
@@ -1,4 +1,5 @@
 package beaverlib.odometry
+
 // Filed adapted from 2898s 2023 Charged Up code
 import edu.wpi.first.math.Matrix
 import edu.wpi.first.math.Nat
@@ -8,19 +9,24 @@ import edu.wpi.first.math.numbers.N3
 import edu.wpi.first.wpilibj2.command.SubsystemBase
 import frc.robot.beaverlib.misc.BiSignal
 import frc.robot.beaverlib.odometry.BeaverVisionCamera
-import org.photonvision.targeting.PhotonPipelineResult
 import java.util.*
+import org.photonvision.targeting.PhotonPipelineResult
 
 /**
- * A Vision SubsystemBase, that polls [cameras] each frame, and updates [listeners] with the apriltag results
+ * A Vision SubsystemBase, that polls [cameras] each frame, and updates [listeners] with the
+ * apriltag results
+ *
  * @param cameras A list of all of the cameras connected to pi, to pull for apriltag results
  */
 class BeaverPhotonVision(vararg val cameras: BeaverVisionCamera) : SubsystemBase() {
-    /** Runs each of the Lambdas with the apriltag results, and the camera they came from, for every apriltag result. */
+    /**
+     * Runs each of the Lambdas with the apriltag results, and the camera they came from, for every
+     * apriltag result.
+     */
     val listeners = BiSignal<PhotonPipelineResult, BeaverVisionCamera>()
 
     override fun periodic() {
-        for(camera in cameras){
+        for (camera in cameras) {
             for (result in camera.results) {
                 listeners.update(result, camera)
             }
@@ -28,22 +34,25 @@ class BeaverPhotonVision(vararg val cameras: BeaverVisionCamera) : SubsystemBase
     }
 
     /** Updates all of the cameras pose estimators with the given reference pose */
-    fun setAllCameraReferences(pose: Pose2d) {
-        for(camera in cameras) {
-            camera.setReference(pose)
+    fun setAllCameraReferences(pose: Pose3d) {
+        for (camera in cameras) {
+            camera.referencePose = pose
         }
     }
 
-    /** Returns the standard deviation matrix required by YAGSL given each of the standard deviation inputs
+    /**
+     * Returns the standard deviation matrix required by YAGSL given each of the standard deviation
+     * inputs
+     *
      * @param STDVX Standard deviation in the X estimation
      * @param STDVY Standard deviation in the Y estimation
      * @param rotationSTD Standard deviation in the rotation estimation
-     * */
-    fun getStandardDev(STDVX : Double, STDVY : Double, rotationSTD: Double): Matrix<N3, N1>{
+     */
+    fun getStandardDev(STDVX: Double, STDVY: Double, rotationSTD: Double): Matrix<N3, N1> {
         val stdv = Matrix(Nat.N3(), Nat.N1())
-        stdv.set(0,0, STDVX)
-        stdv.set(1,0, STDVY)
-        stdv.set(2,0, rotationSTD)
+        stdv.set(0, 0, STDVX)
+        stdv.set(1, 0, STDVY)
+        stdv.set(2, 0, rotationSTD)
         return stdv
     }
 }
