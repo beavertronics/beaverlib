7737b94137b9ee26d4eaaee69e5023767746720d
BubbleShade <ozypfl@gmail.com>
2026-02-27 20:21:34 -0800

    Fix Polynomial

--- utils/Polynomial.kt
+++ utils/Polynomial.kt
@@ -1,16 +1,22 @@
 package frc.engine.utils
+
 // File adapted from 2898's bpsrobotics engine
 import kotlin.math.pow
 
-class Polynomial(vararg coefficients: Double) {
-    val coef = coefficients
-
-    fun calculate(value: Double) : Double {
+/**
+ * Creates a polynomial using the given coefficients
+ *
+ * @param coefficients the coefficients of the terms of the polynomial, in order of greatest power
+ *   of x to least
+ */
+class Polynomial(vararg val coefficients: Double) {
+    fun calculate(value: Double): Double {
         var returnValue = 0.0
-        for (i: Int in 0..coef.size-1){
-            returnValue += coef[i]*(value.pow(coef.size-i))
+        for (i: Int in 0..<coefficients.size) {
+            returnValue += coefficients[i] * (value.pow(coefficients.size - i - 1))
         }
         return returnValue
     }
-    fun calculate(value: Int) : Double = calculate(value.toDouble())
+
+    fun calculate(value: Int): Double = calculate(value.toDouble())
 }
