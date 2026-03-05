884fd251cb2d5c0c3d788de980a5ab04903350bb
BubbleShade <ozypfl@gmail.com>
2026-03-05 10:43:10 -0800

    Update MovingAverage.kt

--- utils/MovingAverage.kt
+++ utils/MovingAverage.kt
@@ -1,7 +1,8 @@
 package beaverlib.utils
+
 // File adapted from 2898's bpsrobotics engine
-import beaverlib.utils.Units.Frequency
-import beaverlib.utils.Units.hertz
+
+import kotlin.math.sign
 import kotlin.reflect.KProperty
 
 @Suppress("NOTHING_TO_INLINE")
@@ -11,12 +12,28 @@ class MovingAverage(val size: Int) {
 
     var average = 0.0
         private set
-    val median get() = buffer.sorted()[buffer.size / 2]
 
-    fun add(v: Double) {
+    val median
+        get() = buffer.sorted()[buffer.size / 2]
+
+    fun add(v: Double): Double {
         buffer[i++ % buffer.size] = v
         average = buffer.average()
+        return average
+    }
+
+    /** Sets all values in the buffer equal to v */
+    fun clear(v: Double = 0.0) {
+        buffer.fill(v)
     }
 
     inline operator fun getValue(thisRef: Any?, property: KProperty<*>): Double = average
+
+    inline operator fun invoke(v: Double): Double {
+        return add(v)
+    }
+
+    inline operator fun compareTo(other: Number): Int {
+        return sign(average - other.toDouble()).toInt()
+    }
 }
